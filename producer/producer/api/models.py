8fb8d4aada154bf82140d8ad2855116ef664686f
Boris A. Burkov <vasjaforutube@gmail.com>
2018-07-25 22:34:51 +0100

    Templates for model/serializer/view for jobs.

--- producer/producer/api/models.py
+++ producer/producer/api/models.py
@@ -4,4 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-
+class Job(models.Model):
+    id = models.CharField(max_length=36, primary_key=True)
+    query = models.TextField()
+    submitted = models.DateTimeField(null=True)
+    finished = models.DateTimeField(null=True, blank=True)
--- producer/producer/api/views.py
+++ producer/producer/api/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from serializers import JobSerializer
+
+
+class SubmitJob(APIView):
+    def post(self, request, format=None):
+        serializer = JobSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
