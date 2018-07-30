bfbc87c0ffb0b804129589e3d9990fa992c954a7
Boris A. Burkov <vasjaforutube@gmail.com>
2018-07-30 16:07:57 +0100

    Opened 8000 port for django development server.

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,6 +45,13 @@ resource "openstack_compute_secgroup_v2" "terraform" {
   }
 
   rule {
+    from_port = 8000
+    to_port = 8000
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+
+  rule {
     from_port = -1
     to_port = -1
     ip_protocol = "icmp"
