dc3aa2ed0d375f1f60b96d1d7a1baa27b4d9368d
Boris A. Burkov <vasjaforutube@gmail.com>
2018-08-14 17:52:40 +0100

    Moved App code to Server.run.

--- consumer/consumer/main.py
+++ consumer/consumer/main.py
@@ -1,52 +1,18 @@
 import logging
 import sys
 
-import aiohttp_jinja2
-import jinja2
-from aiohttp import web
 
-# from consumer.db import close_pg, init_pg
-from consumer.middlewares import setup_middlewares
-from consumer.urls import setup_routes
 from consumer.settings import get_config
-
-
-async def init_app(argv=None):
-    app = web.Application()
-
-    app['config'] = get_config(argv)
-
-    # setup Jinja2 template renderer
-    aiohttp_jinja2.setup(
-        app, loader=jinja2.PackageLoader('aiohttpdemo_polls', 'templates')
-    )
-
-    # create db connection on startup, shutdown on exit
-    # app.on_startup.append(init_pg)
-    # app.on_cleanup.append(close_pg)
-
-    # setup views and routes
-    setup_routes(app)
-
-    # setup middlewares
-    setup_middlewares(app)
-
-    return app
+from consumer.server import Server
 
 
 def main(argv):
     logging.basicConfig(level=logging.DEBUG)
 
-    app = init_app(argv)
-
     config = get_config(argv)
 
-    web.run_app(
-        app,
-        host=config['host'],
-        port=config['port']
-    )
-
+    server = Server(config['host'], config['port'])
+    server.run()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- consumer/consumer/server.py
+++ consumer/consumer/server.py
@@ -2,11 +2,40 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from collections import deque
 
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+
+# from consumer.db import close_pg, init_pg
+# from consumer.middlewares import setup_middlewares
+from consumer.urls import setup_routes
+
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, loop):
         self.host = host
         self.port = port
-        self.pool = ThreadPoolExecutor(max_workers=10)
+
+        # Why using thread pool at all? Because there can be blocking calls: https://pymotw.com/3/asyncio/executors.html
+        self.pool = ThreadPoolExecutor(max_workers=1)
         self.loop = asyncio.get_event_loop()
         self.data = deque([])
+
+    def run(self):
+        app = web.Application()
+        app['config'] = config
+
+        # setup Jinja2 template renderer
+        aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('aiohttpdemo_polls', 'templates'))
+
+        # create db connection on startup, shutdown on exit
+        # app.on_startup.append(init_pg)
+        # app.on_cleanup.append(close_pg)
+
+        # setup views and routes
+        setup_routes(app)
+
+        # setup middlewares
+        # setup_middlewares(app)
+
+        web.run_app(app, host=config['host'], port=config['port'])
