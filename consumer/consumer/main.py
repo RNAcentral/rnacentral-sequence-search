c033f4526d8f7e616336715114541d32a5ca4d40
Boris A. Burkov <vasjaforutube@gmail.com>
2018-08-24 17:47:24 +0100

    Fixed job url and added a docstring.

--- consumer/consumer/main.py
+++ consumer/consumer/main.py
@@ -5,6 +5,12 @@ from .settings import get_config
 from .server import Server
 
 
+"""
+Run this with:
+
+python3 -m consumer.main
+"""
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
--- consumer/consumer/urls.py
+++ consumer/consumer/urls.py
@@ -1,5 +1,3 @@
-import pathlib
-
 from .views import index, submit_job, result
 from .settings import PROJECT_ROOT
 
@@ -7,7 +5,7 @@ from .settings import PROJECT_ROOT
 def setup_routes(app):
     app.router.add_get('/', index, name='index')
     app.router.add_get('/results/{result_id}', result, name='result')
-    app.router.add_post('/job/{job_id}', submit_job, name='submit_job')
+    app.router.add_post('/job', submit_job, name='submit_job')
     setup_static_routes(app)
 
 
