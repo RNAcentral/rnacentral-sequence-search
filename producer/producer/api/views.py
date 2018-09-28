fabc2e8a833c9648b70f4d519bda631b3ac0d1f8
Boris A. Burkov <vasjaforutube@gmail.com>
2018-09-28 23:11:45 +0100

    Make a long list of per-database consumers.

--- producer/producer/api/views.py
+++ producer/producer/api/views.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from kafka import KafkaProducer
 
 from serializers import JobSerializer
 
@@ -15,10 +14,8 @@ class SubmitJob(APIView):
         serializer = JobSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-
-            producer = KafkaProducer(bootstrap_servers=["kafka:9092"])
-            # produce keyed messages to enable hashed partitioning
-            producer.send(settings.KAFKA_TOPICS['tRNA'], key=b'foo', value=b'bar')
+            for database in request.data["databases"]:
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
