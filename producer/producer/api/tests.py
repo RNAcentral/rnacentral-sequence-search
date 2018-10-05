cd3d8c1be36dff04c17871d18ddfbee0a1d67205
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-05 16:42:26 +0100

    A unit-test for producer job-done endpoint.

--- producer/producer/api/tests.py
+++ producer/producer/api/tests.py
@@ -18,13 +18,22 @@ from django.conf import settings
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase, APIClient
 
+from .models import Job
+
 
 class JobDone(APITestCase):
-    """Basic tests for generic endpoints."""
-    def test_current_api_endpoint(self):
-        """Stable endpoint for the latest version of the API."""
+    def test_submit_job_post_success(self):
+        c = APIClient()
+        url = reverse('submit-job')
+        response = c.post(url, data={"query": "ACGCTGATGCTAGTGCGATGCTA", "databases": ["mirbase"]})
+        self.assertEqual(response.status_code, 201)
+
+    def test_job_done_post_success(self):
+        # prepare job object to submit this job chunk into it
+        job = Job.objects.create(query="ACGCTGATGCTAGTGCGATGCTA", databases=["mirbase"])
+
         c = APIClient()
         url = reverse('job-done')
-        response = c.post(url, data={"job_id": 1, "database": "mirbase", "result": "asdfjasdjfajsdfjasjdfjasdjfajsdf"})
-        self.assertEqual(response.status_code, 200)
-
+        response = c.post(url, data={"job_id": job.id, "database": "mirbase", "result": "asdfjasdjfajsdfjasjdfjasdjfa"})
+        if response.status_code != 201:
+            self.assertEqual(response.status_code, 201)
