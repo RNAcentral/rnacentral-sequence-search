f1a3b6a1a74bfd360eca23bc0376fecab7e6c4a8
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-07 12:22:29 +0100

    Database connection initialization on startup, moved migrations to __main__.

--- producer/producer/main.py
+++ producer/producer/main.py
@@ -19,17 +19,16 @@ import jinja2
 from aiojobs.aiohttp import setup as setup_aiojobs
 from aiohttp import web, web_middlewares
 
-# from consumer.db import close_pg, init_pg
-# from consumer.middlewares import setup_middlewares
-from .urls import setup_routes
 from . import settings
+from .models import init_pg, close_pg
+from .urls import setup_routes
 
 """
 Run either of the following commands from the parent of current directory:
 
-adev runserver consumer --livereload
+adev runserver producer --livereload
 
-python3 -m consumer.main
+python3 -m producer.main
 """
 
 
@@ -40,14 +39,14 @@ def create_app():
         web_middlewares.normalize_path_middleware(append_slash=True),
     ])
 
-    app.update(name='consumer', settings=settings)
+    app.update(name='producer', settings=settings)
 
     # setup Jinja2 template renderer
     aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('consumer', 'templates'))
 
     # create db connection on startup, shutdown on exit
-    # app.on_startup.append(init_pg)
-    # app.on_cleanup.append(close_pg)
+    app.on_startup.append(init_pg)
+    app.on_cleanup.append(close_pg)
 
     # setup views and routes
     setup_routes(app)
@@ -62,10 +61,8 @@ def create_app():
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
     app = create_app()
-
-    web.run_app(app, host=settings.HOST, port=settings.PORT)
+    web.run_app(app, host=app['settings'].HOST, port=app['settings'].PORT)
 
     # Why using thread pool at all? Because there can be blocking calls: https://pymotw.com/3/asyncio/executors.html
     # pool = ThreadPoolExecutor(max_workers=1)
--- producer/producer/models.py
+++ producer/producer/models.py
@@ -11,8 +11,35 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from aiohttp import web
 import sqlalchemy as sa
+from aiopg.sa import create_engine
 
+from .main import create_app
+
+
+# Connection initialization code
+# ------------------------------
+
+async def init_pg(app):
+    engine = await create_engine(
+        user=app['POSTGRES_USER'],
+        database=app['POSTGRES_DATABASE'],
+        host=app['POSTGRES_HOST'],
+        password=app['POSTGRES_PASSWORD']
+    )
+
+    async with engine:
+        app['connection'] = await engine.acquire()
+
+
+async def close_pg(app):
+    app['connection'].close()
+    await app['connection'].wait_closed()
+
+
+# Models schema
+# -------------
 
 STATUS_CHOICES = (
     ('started', 'started'),
@@ -25,10 +52,10 @@ metadata = sa.MetaData()
 
 jobs = sa.Table('jobs', metadata,
                  sa.Column('id', sa.Integer, primary_key=True),
-                 sa.Colum('query', sa.Text),
+                 sa.Column('query', sa.Text),
                  sa.Column('databases', sa.String(255)),  # should be array of Strings with choices=DATABASE_CHOICES
                  sa.Column('submitted', sa.DateTime),
-                 sa.Column('finished', sa.DateTime, nullable),
+                 sa.Column('finished', sa.DateTime, nullable=True),
                  sa.Column('status', sa.String(255)))  # choices=STATUS_CHOICES, default='started'
 
 """Part of the search job, run against a specific database and assigned to a specific consumer"""
@@ -40,24 +67,31 @@ job_chunks = sa.Table('job_chunks', metadata,
                   sa.Column('status', sa.String(255), nullable=False))  # choices=STATUS_CHOICES, default='started'
 
 
-async def migrate(connection):
-    await connection.execute('DROP TABLE IF EXISTS jobs')
-    await connection.execute('DROP TABLE IF EXISTS job_chunks')
-    await connection.execute('''
-        CREATE TABLE jobs (
-          id serial PRIMARY KEY,
-          query TEXT,
-          databases VARCHAR(255),
-          submitted TIMESTAMP,
-          finished TIMESTAMP,
-          status VARCHAR(255))
-    ''')
+# Migrations
+# ----------
 
-    await connection.execute('''
-        CREATE TABLE job_chunks (
-          id serial,
-          job_id int references jobs(id),
-          database VARCHAR(255),
-          result VARCHAR(255)
-          status VARCHAR(255))
-    ''')
+if __name__ == "__main__":
+    async def migrate(connection):
+        await connection.execute('DROP TABLE IF EXISTS jobs')
+        await connection.execute('DROP TABLE IF EXISTS job_chunks')
+        await connection.execute('''
+            CREATE TABLE jobs (
+              id serial PRIMARY KEY,
+              query TEXT,
+              databases VARCHAR(255),
+              submitted TIMESTAMP,
+              finished TIMESTAMP,
+              status VARCHAR(255))
+        ''')
+
+        await connection.execute('''
+            CREATE TABLE job_chunks (
+              id serial,
+              job_id int references jobs(id),
+              database VARCHAR(255),
+              result VARCHAR(255)
+              status VARCHAR(255))
+        ''')
+
+        app = create_app()
+        web.run_app(app, host=app['settings'].HOST, port=app['settings'].PORT)
