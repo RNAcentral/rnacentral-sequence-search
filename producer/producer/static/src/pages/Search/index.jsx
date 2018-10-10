9b55317a104ba48c85378815dc907abb1b10fe05
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-10 21:04:30 +0100

    Form input stub.

--- producer/producer/static/src/pages/Search/index.jsx
+++ producer/producer/static/src/pages/Search/index.jsx
@@ -7,10 +7,29 @@ class Search extends React.Component {
         <div className="col-lg-12">
           <div className="hpanel">
             <div className="panel-heading">
-              <h1>Home</h1>
+              <h1>Submit an RNA sequence:</h1>
             </div>
             <div className="panel-body">
-              <p>Content of homepage.</p>
+              <form>
+                <div className="jd_toolParameterBox">
+                  <fieldset>
+                    <legend>STEP 1 - Enter your input sequences</legend>
+                    <p>
+                      <label htmlFor="sequence">
+                        <a href="https://www.ebi.ac.uk/seqdb/confluence/display/THD/MUSCLE#MUSCLE-sequence" target="_help">Enter or paste</a>
+                      </label> a set of sequences in any supported <a href="https://www.ebi.ac.uk/seqdb/confluence/display/THD/MUSCLE#MUSCLE-sequence" target="_help">format</a>:
+                    </p>
+                    <textarea cols="47" id="sequence" name="sequence" rows="7"></textarea>
+                    <p>
+                      Or <label htmlFor="upfile"> <a href="https://www.ebi.ac.uk/seqdb/confluence/display/THD/MUSCLE#MUSCLE-upload" target="_help">upload</a></label> a file:
+                      <input id="upfile" name="upfile" type="file"/>
+                      <label>
+                        Use a <a href="#" id="exampleSeq">example sequence</a> | <a href="#" id="clearSequence">Clear sequence</a> | <a href="#" id="seeMoreExample">See more example inputs</a>
+                      </label>
+                    </p>
+                  </fieldset>
+                </div>
+              </form>
             </div>
           </div>
         </div>
