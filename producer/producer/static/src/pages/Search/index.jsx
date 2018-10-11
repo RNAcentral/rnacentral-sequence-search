082590004283e24eade93bc380c8de1d18403f43
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-11 10:38:24 +0100

    Code for fetching RNAcentral databases from the backend.

--- producer/producer/static/src/pages/Search/index.jsx
+++ producer/producer/static/src/pages/Search/index.jsx
@@ -1,32 +1,50 @@
 import React from 'react';
 
+import routes from 'services/routes.jsx';
+
+
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rnacentralDatabases: [],
+    };
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-lg-12">
           <div className="hpanel">
             <div className="panel-heading">
-              <h1>Submit an RNA sequence</h1>
+              <h1>Search an RNA sequence in RNA databases</h1>
             </div>
             <div className="panel-body">
               <form>
                 <div className="jd_toolParameterBox">
                   <fieldset>
-                    <legend>Enter your input RNA sequence:</legend>
+                    <h4>RNA sequence:</h4>
                     <p>
-                      <label htmlFor="sequence">
-                        <a href="https://www.ebi.ac.uk/seqdb/confluence/display/THD/MUSCLE#MUSCLE-sequence" target="_help">Enter or paste</a>
-                      </label>
-                    </p>
-                    <textarea cols="47" id="sequence" name="sequence" rows="7"></textarea>
-                    <p>
-                      Or upload a file:
-                      <input id="upfile" name="upfile" type="file"/>
                       <label>
                         Use a <a href="#" id="exampleSeq">example sequence</a> | <a href="#" id="clearSequence">Clear sequence</a> | <a href="#" id="seeMoreExample">See more example inputs</a>
                       </label>
                     </p>
+                    <textarea id="sequence" name="sequence" rows="7"></textarea>
+                    <p>
+                      Or upload a file:
+                      <input id="upfile" name="upfile" type="file"/>
+                    </p>
+                  </fieldset>
+                </div>
+                <div className="jd_toolParameterBox">
+                  <fieldset>
+                    <h4>RNA databases:</h4>
+                    <ul className="vertical menu facets">
+                      {this.state.rnacentralDatabases.map(database =>
+                        <li key={database}><span className="facet"><input id={database} type="checkbox" /><label htmlFor={database}>{database}</label></span></li>
+                      )}
+                    </ul>
                   </fieldset>
                 </div>
               </form>
@@ -36,6 +54,13 @@ class Search extends React.Component {
       </div>
     )
   }
+
+  componentDidMount() {
+    fetch(routes.rnacentalDatabases)
+      .then(response => response.json())
+      .then(data => this.setState({ rnacentralDatabases: data.rnacentralDatabases }));
+  }
+
 }
 
 export default Search;
---
+++ producer/producer/static/src/services/routes.jsx
@@ -0,0 +1,5 @@
+module.exports = {
+  'rnacentalDatabases': '/rnacentral-databases',
+  'search': '',
+  'results': '',
+};
