4b96a53c64d684a515b9642284cfb5841233db92
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-13 19:09:30 +0100

    Status icons on Job page, spin css class.

--- producer/producer/static/src/pages/Job/index.jsx
+++ producer/producer/static/src/pages/Job/index.jsx
@@ -14,12 +14,19 @@ class Job extends React.Component {
     };
 
     this.getStatus = this.getStatus.bind(this);
+    this.displayStatusIcon = this.displayStatusIcon.bind(this);
   }
 
   getStatus() {
     fetch(routes.jobStatus(this.props.match.params.jobId))
       .then(response => response.json())
       .then(data => this.setState(data));
+  }
+
+  displayStatusIcon(status) {
+    if (status === 'success') return (<i className="icon icon-functional" style={{color: "green"}} data-icon="/"/>);
+    else if (status === 'started') return (<i className="icon icon-functional spin" data-icon="s"/>);
+    else if (status === 'error') return (<i className="icon icon-generic" style={{color: "red"}} data-icon="l"/>);
   }
 
   componentDidMount() {
@@ -34,10 +41,10 @@ class Job extends React.Component {
         <div className="col-lg-12">
           <div className="hpanel">
             <div className="panel-heading">
-              <h1>Job {this.props.match.params.jobId}</h1>
+              <h1>Job { this.props.match.params.jobId } { this.displayStatusIcon(this.state.status) }</h1>
             </div>
             <div className="panel-body">
-              {this.state.status}
+              { this.state.chunks.map((chunk, index) => (<div key={index}> { chunk.database }: { this.displayStatusIcon(chunk.status) }</div>)) }
             </div>
           </div>
         </div>
