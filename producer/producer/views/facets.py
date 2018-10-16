39733e632d60993cabb83b8cb6a0809bb48a7d5f
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-16 15:29:11 +0100

    Reordered facets in facets.py.

--- producer/producer/views/facets.py
+++ producer/producer/views/facets.py
@@ -20,49 +20,50 @@ async def facets(request):
     data = {
         "facets": [
             {
-                "id":"TAXONOMY",
-                "label":"Organisms",
-                "total":1,
-                "facetValues":[
-                    {"label":"Homo sapiens","value":"9606","count":1}
+                "id": "rna_type",
+                "label": "RNA types",
+                "total": 1,
+                "facetValues": [{"label": "precursor RNA", "value": "precursor RNA", "count": 1}]
+            },
+            {
+                "id": "TAXONOMY",
+                "label": "Organisms",
+                "total": 1,
+                "facetValues": [
+                    {"label": "Homo sapiens", "value": "9606", "count": 1}
                 ]
             },
             {
-                "id":"qc_warning_found",
-                "label":"QC warning found",
-                "total":1,
-                "facetValues":[{"label":"False","value":"False","count":1}]
-            },
-            {
-                "id":"has_genomic_coordinates",
-                "label":"Genomic mapping",
-                "total":1,
-                "facetValues":[{"label":"Available","value":"True","count":1}]},
-            {
-                "id":"popular_species",
-                "label":"Popular species",
-                "total":1,
-                "facetValues":[{"label":"Homo sapiens","value":"9606","count":1}]
-            },
-            {
-                "id":"rna_type",
-                "label":"RNA types",
-                "total":1,
-                "facetValues":[{"label":"precursor RNA","value":"precursor RNA","count":1}]
-            },
-            {
-                "id":"expert_db",
-                "label":"Expert databases",
-                "total":6,
-                "facetValues":[
-                    {"label":"HGNC","value":"HGNC","count":1},
-                    {"label":"RefSeq","value":"RefSeq","count":1},
-                    {"label":"miRBase","value":"miRBase","count":1},
-                    {"label":"GENCODE","value":"GENCODE","count":1},
-                    {"label":"Ensembl","value":"Ensembl","count":1},
-                    {"label":"ENA","value":"ENA","count":1}
+                "id": "expert_db",
+                "label": "Expert databases",
+                "total": 6,
+                "facetValues": [
+                    {"label": "HGNC", "value": "HGNC", "count": 1},
+                    {"label": "RefSeq", "value": "RefSeq", "count": 1},
+                    {"label": "miRBase", "value": "miRBase", "count": 1},
+                    {"label": "GENCODE", "value": "GENCODE", "count": 1},
+                    {"label": "Ensembl", "value": "Ensembl", "count": 1},
+                    {"label": "ENA", "value": "ENA", "count": 1}
                 ]
-            }
+            },
+            {
+                "id": "qc_warning_found",
+                "label": "QC warning found",
+                "total": 1,
+                "facetValues": [{"label": "False", "value": "False", "count": 1}]
+            },
+            {
+                "id": "has_genomic_coordinates",
+                "label": "Genomic mapping",
+                "total": 1,
+                "facetValues": [{"label": "Available", "value": "True", "count": 1}]
+            },
+            # {
+            #     "id": "popular_species",
+            #     "label": "Popular species",
+            #     "total": 1,
+            #     "facetValues": [{"label": "Homo sapiens", "value": "9606", "count": 1}]
+            # }
         ]
     }
 
