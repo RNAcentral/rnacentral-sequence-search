5524705e9ac9559fbc67ee07282d8276872d7750
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-16 10:10:26 +0100

    Removed pagination templates from Result page, going to use infinite scroll instead.

--- producer/producer/static/src/pages/Result/index.jsx
+++ producer/producer/static/src/pages/Result/index.jsx
@@ -41,18 +41,6 @@ class Result extends React.Component {
             <ul key={result}><Hit result={result} alignmentsCollapsed={this.state.alignmentsCollapsed} onToggleAlignmentsCollapsed={ this.onToggleAlignmentsCollapsed } /></ul>
             )) }
           </section>
-          <ul className="pagination" role="navigation" aria-label="Pagination">
-            <li className="pagination-previous disabled">Previous <span className="show-for-sr">page</span></li>
-            <li className="current"><span className="show-for-sr">You're on page</span> 1</li>
-            <li><a href="#" aria-label="Page 2">2</a></li>
-            <li><a href="#" aria-label="Page 3">3</a></li>
-            <li><a href="#" aria-label="Page 4">4</a></li>
-            <li className="ellipsis" aria-hidden="true"></li>
-            <li><a href="#" aria-label="Page 12">12</a></li>
-            <li><a href="#" aria-label="Page 13">13</a></li>
-            <li className="pagination-next"><a href="#" aria-label="Next page">Next <span
-              className="show-for-sr">page</span></a></li>
-          </ul>
         </div>
         <Facets resultId={this.props.match.params.resultId}></Facets>
       </div>
