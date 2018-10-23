6cdf53cccec7a2f1f6f3f73f528f208d0657a4ef
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-23 17:26:36 +0100

    Added volumes and attaches to terraform config.

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,6 +112,19 @@ resource "openstack_compute_instance_v2" "consumer" {
   }
 }
 
+resource "openstack_blockstorage_volume_v2" "sequence_search_databases" {
+  count = 10
+  size = 10
+  name = "sequence-search-databases-${count.index + 1}"
+  image_id = "sequence_search_databases"
+}
+
+resource "openstack_compute_volume_attach_v2" "attach" {
+  count=10
+  instance_id = "${openstack_compute_instance_v2.consumer.id[count.index + 1]}"
+  volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_databases.id[count.index + 1]}"
+}
+
 resource "openstack_compute_floatingip_associate_v2" "sequence_search" {
   depends_on = ["openstack_compute_instance_v2.producer", "openstack_networking_router_interface_v2.sequence_search"]
   floating_ip = "${var.floating_ip}"
