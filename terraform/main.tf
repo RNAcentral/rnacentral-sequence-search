1b67fa829f3e3bc9c0a87bc5ec1b3be7535b838f
Boris A. Burkov <vasjaforutube@gmail.com>
2018-10-23 20:50:41 +0100

    Count syntax fixes in main.tf.

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,8 +121,8 @@ resource "openstack_blockstorage_volume_v2" "sequence_search_databases" {
 
 resource "openstack_compute_volume_attach_v2" "attach" {
   count=10
-  instance_id = "${openstack_compute_instance_v2.consumer.id[count.index + 1]}"
-  volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_databases.id[count.index + 1]}"
+  instance_id = "${openstack_compute_instance_v2.consumer.*.id[count.index]}"
+  volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_databases.*.id[count.index]}"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "sequence_search" {
