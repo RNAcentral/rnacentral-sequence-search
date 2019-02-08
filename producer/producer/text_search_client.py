607799dfc77879c244f9691e90aea128837035b3
Boris A. Burkov <vasjaforutube@gmail.com>
2019-02-08 16:53:28 +0000

    Abstracted-out interaction with text search into text_search_client.py.

---
+++ producer/producer/text_search_client.py
@@ -0,0 +1,131 @@
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import aiohttp
+
+from .settings import EBI_SEARCH_PROXY_URL
+
+
+# if we fail to retrieve facets,
+facets_stub = {
+    "facets": [
+        {
+            "id": "rna_type",
+            "label": "RNA types",
+            "total": 0,
+            "facetValues": []
+        },
+        {
+            "id": "TAXONOMY",
+            "label": "Organisms",
+            "total": 0,
+            "facetValues": []
+        },
+        {
+            "id": "expert_db",
+            "label": "Expert databases",
+            "total": 0,
+            "facetValues": []
+        },
+        {
+            "id": "qc_warning_found",
+            "label": "QC warning found",
+            "total": 0,
+            "facetValues": []
+        },
+        {
+            "id": "has_genomic_coordinates",
+            "label": "Genomic mapping",
+            "total": 0,
+            "facetValues": []
+        }
+    ]
+}
+
+
+class ProxyConnectionError(Exception):
+    """Raised when we failed to write text search results to our proxy"""
+    def __str__(self):
+        return "Proxy connection error"
+
+
+class EBITextSearchConnectionError(Exception):
+    """Raised when we failed to retrieve text search results from EBI search"""
+    def __str__(self):
+        return "EBI text search connection error"
+
+
+async def get_facets(results, job_id, query, page, page_size):
+    """
+    Post text search results to our proxy, so that EBI_SEARCH could retrieve it and index.
+    Request text search facets from EBI_SEARCH, and return those data.
+    """
+    # send the list of rnacentral_ids to the proxy, fallback to returning the plain results, if text search unavailable
+    rnacentral_ids = "\n".join([result['rnacentral_id'] for result in results])
+    url = EBI_SEARCH_PROXY_URL + '/' + job_id
+    headers = {'content-type': 'text/plain'}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=rnacentral_ids, headers=headers) as response:
+            if response.status >= 400:
+                raise ProxyConnectionError(response)
+
+    # request facets from ebi text search
+    fields = [
+        'active',
+        'author',
+        'common_name',
+        'description',
+        'expert_db',
+        'function',
+        'gene',
+        'gene_synonym',
+        'has_genomic_coordinates',
+        'length',
+        'locus_tag',
+        'organelle',
+        'pub_title',
+        'product',
+        'qc_warning_found',
+        'qc_warning',
+        'rna_type',
+        'standard_name',
+        'tax_string'
+    ]
+
+    facetfields = [
+        'length',
+        'rna_type',
+        'TAXONOMY',
+        'expert_db',
+        'qc_warning_found',
+        'has_genomic_coordinates',
+        'popular_species'
+    ]
+
+    url = "http://wwwdev.ebi.ac.uk/ebisearch/ws/rest/rnacentral/seqtoolresults/" \
+          "?toolid=rnac_nhmmer" \
+          "&jobid={job_id}" \
+          "&query={query}" \
+          "&format=json&fields={fields}" \
+          "&facetcount={facetcount}" \
+          "&facetfields={facetfields}" \
+          "&size={page_size}" \
+          "&start={page}" \
+        .format(job_id=job_id, query=query, fields=','.join(fields), facetcount=30, facetfields=','.join(facetfields),
+                page=page, page_size=page_size)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status < 400:
+                return await response.json()
--- producer/producer/views/facets_search.py
+++ producer/producer/views/facets_search.py
@@ -12,12 +12,11 @@ limitations under the License.
 """
 
 import logging
-import aiohttp
-from aiohttp import web
-import sqlalchemy as sa
 
-from ..models import Job, JobChunk, JobChunkResult
+from aiohttp import web
+
 from ..db.jobs import get_job_results
+from ..text_search_client import get_facets, facets_stub, ProxyConnectionError, EBITextSearchConnectionError
 
 
 async def facets_search(request):
@@ -43,6 +42,8 @@ async def facets_search(request):
       '502':
         description: Could not connect to EBI search proxy
     """
+    logger = logging.getLogger('aiohttp.web')
+
     job_id = request.match_info['job_id']
 
     query = request.query['query'] if 'query' in request.query else 'rna'
@@ -50,81 +51,22 @@ async def facets_search(request):
     page_size = request.query['page_size'] if 'page_size' in request.query else 20
 
     # get sequence search results from the database, sort/aggregate?
-    results = get_job_results(request.app['engine'], job_id)
     # TODO: sort/aggregate sequence search results?
+    results = await get_job_results(request.app['engine'], job_id)
 
-    # send the list of rnacentral_ids to the proxy, fallback to returning the plain results, if text search unavailable
-    rnacentral_ids = "\n".join([result['rnacentral_id'] for result in results])
-    url = request.app['settings'].EBI_SEARCH_PROXY_URL + '/' + job_id
-    headers = {'content-type': 'text/plain'}
+    # try to get facets from EBI text search, otherwise stub facets
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, data=rnacentral_ids, headers=headers) as response:
-                if response.status >= 400:
-                    raise web.HTTPBadGateway(text="Couldn't connect to text search proxy")
-    except Exception as e:
-        logging.error(str(e))
-        return web.HTTPBadGateway(text=str(e))
+        facets = await get_facets(results, job_id, query, page, page_size)
+    except (ProxyConnectionError, EBITextSearchConnectionError) as e:
+        # text search is not available, pad output with facets stub, indicate that
+        logger.warning(str(e))
+        facets = facets_stub
 
-    # request facets from ebi text search
-    fields = [
-        'active',
-        'author',
-        'common_name',
-        'description',
-        'expert_db',
-        'function',
-        'gene',
-        'gene_synonym',
-        'has_genomic_coordinates',
-        'length',
-        'locus_tag',
-        'organelle',
-        'pub_title',
-        'product',
-        'qc_warning_found',
-        'qc_warning',
-        'rna_type',
-        'standard_name',
-        'tax_string'
-    ]
+    # inject text search results into facets json
+    for entry in facets['entries']:
+        for result in results:
+            if result['rnacentral_id'] == entry['id']:
+                entry.update(result)
 
-    facetfields = [
-        'length',
-        'rna_type',
-        'TAXONOMY',
-        'expert_db',
-        'qc_warning_found',
-        'has_genomic_coordinates',
-        'popular_species'
-    ]
+    return web.json_response(facets)
 
-    url = "http://wwwdev.ebi.ac.uk/ebisearch/ws/rest/rnacentral/seqtoolresults/" \
-          "?toolid=rnac_nhmmer" \
-          "&jobid={job_id}" \
-          "&query={query}" \
-          "&format=json&fields={fields}" \
-          "&facetcount={facetcount}" \
-          "&facetfields={facetfields}" \
-          "&size={page_size}" \
-          "&start={page}" \
-        .format(job_id=job_id, query=query, fields=','.join(fields), facetcount=30, facetfields=','.join(facetfields), page=page, page_size=page_size)
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                if response.status >= 400:
-                    raise web.HTTPBadGateway(text=response.status)
-                else:
-                    text_search_data = await response.json()
-
-                    # inject sequence search data into text_search_data
-                    for entry in text_search_data['entries']:
-                        for result in results:
-                            if result['rnacentral_id'] == entry['id']:
-                                entry.update(result)
-
-                    return web.json_response(text_search_data)
-    except Exception as e:
-        logging.error(str(e))
-        return web.HTTPBadGateway(text=str(e))
