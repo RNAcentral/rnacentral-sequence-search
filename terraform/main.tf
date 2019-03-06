f0b92a253a0b0b4ea8e2a11e48adc1b502c6c2fe
Boris A. Burkov <vasjaforutube@gmail.com>
2019-03-06 11:12:30 +0000

    Attach databases volume to producer, too.

--- terraform/main.tf
+++ terraform/main.tf
@@ -126,17 +126,28 @@ resource "openstack_compute_instance_v2" "consumer" {
   }
 }
 
-resource "openstack_blockstorage_volume_v2" "sequence_search_databases" {
+resource "openstack_blockstorage_volume_v2" "sequence_search_consumer_databases" {
   count = 10
   size = 10
-  name = "sequence-search-databases-${count.index + 1}"
+  name = "sequence-search-consumer-databases-${count.index + 1}"
   image_id = "sequence_search_databases"
 }
 
-resource "openstack_compute_volume_attach_v2" "attach" {
-  count=10
+resource "openstack_compute_volume_attach_v2" "attach_databases_to_consumers" {
+  count = 10
   instance_id = "${openstack_compute_instance_v2.consumer.*.id[count.index]}"
-  volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_databases.*.id[count.index]}"
+  volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_consumer_databases.*.id[count.index]}"
+}
+
+resource "openstack_blockstorage_volume_v2" "sequence_search_producer_databases" {
+  size = 10
+  name = "sequence-search-producer-databases"
+  image_id = "sequence_search_databases"
+}
+
+resource "openstack_compute_volume_attach_v2" "attach_databases_to_producer" {
+  instance_id = "${openstack_compute_instance_v2.producer.id}"
+  volume_id = "${openstack_blockstorage_volume_v2.sequence_search_producer_databases.id}"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "sequence_search" {
