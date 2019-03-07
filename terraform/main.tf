af9182e6e31ad70de1ce7ca03de1d476587a915a
Boris A. Burkov <vasjaforutube@gmail.com>
2019-03-07 17:16:31 +0000

    Terraform volumes should not use images with databases directly.

--- terraform/main.tf
+++ terraform/main.tf
@@ -130,19 +130,17 @@ resource "openstack_blockstorage_volume_v2" "sequence_search_consumer_databases"
   count = 10
   size = 10
   name = "sequence-search-consumer-databases-${count.index + 1}"
-  image_id = "sequence_search_databases"
 }
 
 resource "openstack_compute_volume_attach_v2" "attach_databases_to_consumers" {
-  count = 10
+  count = 12
   instance_id = "${openstack_compute_instance_v2.consumer.*.id[count.index]}"
   volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_consumer_databases.*.id[count.index]}"
 }
 
 resource "openstack_blockstorage_volume_v2" "sequence_search_producer_databases" {
-  size = 10
+  size = 12
   name = "sequence-search-producer-databases"
-  image_id = "sequence_search_databases"
 }
 
 resource "openstack_compute_volume_attach_v2" "attach_databases_to_producer" {
