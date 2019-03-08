ba6f0bfa6f58a546d2ed71ad1f2cc6bd32b6b2fd
Boris A. Burkov <vasjaforutube@gmail.com>
2019-03-08 15:53:46 +0000

    Fix: confused size and count.

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,12 +128,12 @@ resource "openstack_compute_instance_v2" "consumer" {
 
 resource "openstack_blockstorage_volume_v2" "sequence_search_consumer_databases" {
   count = 10
-  size = 10
+  size = 12
   name = "sequence-search-consumer-databases-${count.index + 1}"
 }
 
 resource "openstack_compute_volume_attach_v2" "attach_databases_to_consumers" {
-  count = 12
+  count = 10
   instance_id = "${openstack_compute_instance_v2.consumer.*.id[count.index]}"
   volume_id   = "${openstack_blockstorage_volume_v2.sequence_search_consumer_databases.*.id[count.index]}"
 }
