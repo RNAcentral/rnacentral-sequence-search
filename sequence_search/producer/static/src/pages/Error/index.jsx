45b3b66012b4abce4b89c40938a9ac6226381544
Boris A. Burkov <vasjaforutube@gmail.com>
2019-04-16 16:01:35 +0100

    #41 Contact us link.

--- sequence_search/producer/static/src/pages/Error/index.jsx
+++ sequence_search/producer/static/src/pages/Error/index.jsx
@@ -13,6 +13,7 @@ class Error extends React.Component {
             <div className="panel-body">
               <div className="callout alert">
                 <h3>Got an error, while delegating the task to consumers.</h3>
+                <p><a href="mailto:rnacentral@gmail.com">Contact us</a> if the problem persists.</p>
               </div>
             </div>
           </div>
