9858cd75da4540d576b4fde417238429e8ff4e97
carlosribas <caduribas@gmail.com>
2019-09-09 16:58:43 +0100

    Endpoint for searches today and last week

--- sequence_search/producer/views/show_searches.py
+++ sequence_search/producer/views/show_searches.py
@@ -10,20 +10,41 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import datetime
 from aiohttp import web
 
 from ...db.models import Job
+
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+LAST_WEEK = datetime.datetime.now() - datetime.timedelta(days=7)
+
+
+def get_results(records):
+    jobs = []
+    for row in records:
+        item = dict(row.items())
+        item['submitted'] = str(item['submitted'])
+        item['finished'] = str(item['finished'])
+        jobs.append(item)
+    return web.json_response(jobs)
 
 
 async def show_searches(request):
     async with request.app['engine'].acquire() as conn:
         cursor = await conn.execute(Job.select())
         records = await cursor.fetchall()
-        jobs = []
-        for row in records:
-            item = dict(row.items())
-            item['submitted'] = str(item['submitted'])
-            item['finished'] = str(item['finished'])
-            jobs.append(item)
+        return get_results(records)
 
-        return web.json_response(jobs)
+
+async def searches_today(request):
+    async with request.app['engine'].acquire() as conn:
+        cursor = await conn.execute(Job.select().where(Job.columns.submitted > YESTERDAY))
+        records = await cursor.fetchall()
+        return get_results(records)
+
+
+async def searches_last_week(request):
+    async with request.app['engine'].acquire() as conn:
+        cursor = await conn.execute(Job.select().where(Job.columns.submitted > LAST_WEEK))
+        records = await cursor.fetchall()
+        return get_results(records)
