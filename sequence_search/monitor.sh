bb38fcd48d936ac8fdf88a1260e30595eecbcddd
carlosribas <caduribas@gmail.com>
2019-10-01 14:46:30 +0100

    Using rnacentral API endpoint

--- sequence_search/monitor.sh
+++ sequence_search/monitor.sh
@@ -8,15 +8,15 @@ set +o allexport
 
 CONTENT_TYPE="Content-Type: application/json"
 DATABASE_AND_QUERY="{\"databases\": [\"mirbase\"], \"query\": \"CUGUACUAUCUACUGUCUCUC\"}"
-HOST_AND_PORT="https://search.rnacentral.org"
+HOST_AND_PORT="https://rnacentral.org/sequence-search-beta"
 
 # Submit a job
-JOB_ID=$(curl -s -H "${CONTENT_TYPE}" -d "${DATABASE_AND_QUERY}" ${HOST_AND_PORT}/api/submit-job | jq -r '.job_id')
+JOB_ID=$(curl -s -H "${CONTENT_TYPE}" -d "${DATABASE_AND_QUERY}" ${HOST_AND_PORT}/submit-job | jq -r '.job_id')
 #echo "The job id: ${JOB_ID}"
 
 # Job status
 # Expected response: "started"
-INITIAL_STATUS=$(curl -s ${HOST_AND_PORT}/api/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
+INITIAL_STATUS=$(curl -s ${HOST_AND_PORT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
 #echo "The initial status: ${INITIAL_STATUS}"
 
 # Waits 5 seconds.
@@ -24,12 +24,12 @@ sleep 5s
 
 # Job status
 # Expected response: "success"
-FINAL_STATUS=$(curl -s ${HOST_AND_PORT}/api/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
+FINAL_STATUS=$(curl -s ${HOST_AND_PORT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
 #echo "The final status: ${FINAL_STATUS}"
 
 # Facets search
 # Expected response: "hitCount = 23 and textSearchError = false"
-FACETS_SEARCH=$(curl -s ${HOST_AND_PORT}/api/facets-search/$JOB_ID | jq '[.hitCount,.textSearchError]')
+FACETS_SEARCH=$(curl -s ${HOST_AND_PORT}/job-results/$JOB_ID | jq '[.hitCount,.textSearchError]')
 HIT_COUNT=$(echo ${FACETS_SEARCH} |  jq '.[0]')
 SEARCH_ERROR=$(echo ${FACETS_SEARCH} |  jq '.[1]')
 #echo $HIT_COUNT
