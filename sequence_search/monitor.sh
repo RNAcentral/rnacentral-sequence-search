93d242e61b760f1021773b90db4226bd70b3fdb9
carlosribas <caduribas@gmail.com>
2019-10-02 17:18:02 +0100

    Use slack to send messages

--- sequence_search/monitor.sh
+++ sequence_search/monitor.sh
@@ -8,47 +8,41 @@ set +o allexport
 
 CONTENT_TYPE="Content-Type: application/json"
 DATABASE_AND_QUERY="{\"databases\": [\"mirbase\"], \"query\": \"CUGUACUAUCUACUGUCUCUC\"}"
-HOST_AND_PORT="https://rnacentral.org/sequence-search-beta"
+HOST="https://rnacentral.org/"
+ENDPOINT="sequence-search-beta"
 
-# Submit a job
-JOB_ID=$(curl -s -H "${CONTENT_TYPE}" -d "${DATABASE_AND_QUERY}" ${HOST_AND_PORT}/submit-job | jq -r '.job_id')
-#echo "The job id: ${JOB_ID}"
+# First check if the $HOST is up
+if curl -s --head  --request GET $HOST | grep "200 OK" > /dev/null; then
+    # Submit a job
+    JOB_ID=$(curl -s -H "${CONTENT_TYPE}" -d "${DATABASE_AND_QUERY}" ${HOST}/${ENDPOINT}/submit-job | jq -r '.job_id')
 
-# Job status
-# Expected response: "started"
-INITIAL_STATUS=$(curl -s ${HOST_AND_PORT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
-#echo "The initial status: ${INITIAL_STATUS}"
+    # Job status
+    # Expected response: "started"
+    INITIAL_STATUS=$(curl -s ${HOST}/${ENDPOINT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
 
-# Waits 5 seconds.
-sleep 5s
+    # Waits 5 seconds.
+    sleep 5s
 
-# Job status
-# Expected response: "success"
-FINAL_STATUS=$(curl -s ${HOST_AND_PORT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
-#echo "The final status: ${FINAL_STATUS}"
+    # Job status
+    # Expected response: "success"
+    FINAL_STATUS=$(curl -s ${HOST}/${ENDPOINT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
 
-# Facets search
-# Expected response: "hitCount = 23 and textSearchError = false"
-FACETS_SEARCH=$(curl -s ${HOST_AND_PORT}/job-results/$JOB_ID | jq '[.hitCount,.textSearchError]')
-HIT_COUNT=$(echo ${FACETS_SEARCH} |  jq '.[0]')
-SEARCH_ERROR=$(echo ${FACETS_SEARCH} |  jq '.[1]')
-#echo $HIT_COUNT
-#echo $SEARCH_ERROR
+    # Facets search
+    # Expected response: "hitCount = 23 and textSearchError = false"
+    FACETS_SEARCH=$(curl -s ${HOST}/${ENDPOINT}/job-results/$JOB_ID | jq '[.hitCount,.textSearchError]')
+    HIT_COUNT=$(echo ${FACETS_SEARCH} |  jq '.[0]')
+    SEARCH_ERROR=$(echo ${FACETS_SEARCH} |  jq '.[1]')
 
-# Send message in case of unexpected result
-if [ "$INITIAL_STATUS" != "started" ] || [ "$FINAL_STATUS" != "success" ] || [ "$HIT_COUNT" != "23" ] || [ "$SEARCH_ERROR" != "false" ]
-then
-#    echo "There is something wrong here!"
-    curl -s --url $SMTP --ssl-reqd \
-      --mail-from $FROM \
-      --mail-rcpt $TO \
-      --user $FROM":"$PASSWORD \
-      -T <(echo -e "From: ${FROM}\nTo: ${TO}\nSubject: API Error\n\n
-      Please check the RNAcentral sequence search. Last output was: \n\n
-      The initial status: ${INITIAL_STATUS}\n\n
-      The final status: ${FINAL_STATUS}\n\n
-      The hit count: ${HIT_COUNT}\n\n
-      The text search error: ${SEARCH_ERROR}\n\n
-      and the expected result was 'started', 'success', '23' and 'false' respectively.")
-    exit
+    # Send message in case of unexpected result
+    if [ "$INITIAL_STATUS" != "started" ] || [ "$FINAL_STATUS" != "success" ] || [ "$HIT_COUNT" != "22" ] || [ "$SEARCH_ERROR" != "false" ]
+    then
+        text="Please check the RNAcentral sequence search. \n The output was: ${INITIAL_STATUS}, ${FINAL_STATUS}, ${HIT_COUNT} and ${SEARCH_ERROR}. \n Expected result: started, success, 22 and false."
+        escapedText=$(echo ${text} | sed 's/"/\"/g' | sed "s/'/\'/g" )
+        json="{\"text\": \"$escapedText\"}"
+        curl -s -d "payload=$json" $WEBHOOK_URL
+        exit
+    fi
+# No worries if it's not up, UptimeRobot will notify the team.
+else
+    echo "${HOST} is DOWN!"
 fi
