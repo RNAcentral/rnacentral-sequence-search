2ab97e1945b34ace00b4ba3c12fe035fa05df60c
carlosribas <caduribas@gmail.com>
2019-10-06 22:32:09 +0100

    Run search on the correct target (test or default).

--- sequence_search/monitor.sh
+++ sequence_search/monitor.sh
@@ -3,33 +3,35 @@
 # -o allexport enables all following variable definitions to be exported.
 # +o allexport disables this feature.
 set -o allexport
-source .conf-file
+source $PWD/.conf-file
 set +o allexport
 
 CONTENT_TYPE="Content-Type: application/json"
 DATABASE_AND_QUERY="{\"databases\": [\"mirbase\"], \"query\": \">sequence-search-test\nCUGUACUAUCUACUGUCUCUC\"}"
-HOST="https://rnacentral.org"
-ENDPOINT="sequence-search-beta"
+
+# Run search on the correct target (test or default).
+# Ansible adds the correct floating_ip to the .conf-file
+HOST="http://$(echo $FLOATING_IP | sed 's/[][]//g'):8002"
 
 # First check if the $HOST is up
 if curl -s --head  --request GET $HOST | grep "200 OK" > /dev/null; then
     # Submit a job
-    JOB_ID=$(curl -s -H "${CONTENT_TYPE}" -d "${DATABASE_AND_QUERY}" ${HOST}/${ENDPOINT}/submit-job | jq -r '.job_id')
+    JOB_ID=$(curl -s -H "${CONTENT_TYPE}" -d "${DATABASE_AND_QUERY}" ${HOST}/api/submit-job | jq -r '.job_id')
 
     # Job status
     # Expected response: "started"
-    INITIAL_STATUS=$(curl -s ${HOST}/${ENDPOINT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
+    INITIAL_STATUS=$(curl -s ${HOST}/api/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
 
     # Waits 5 seconds.
     sleep 5s
 
     # Job status
     # Expected response: "success"
-    FINAL_STATUS=$(curl -s ${HOST}/${ENDPOINT}/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
+    FINAL_STATUS=$(curl -s ${HOST}/api/job-status/$JOB_ID | jq -r '.chunks | .[] | .status')
 
     # Facets search
     # Expected response: "hitCount = 23 and textSearchError = false"
-    FACETS_SEARCH=$(curl -s ${HOST}/${ENDPOINT}/job-results/$JOB_ID | jq '[.hitCount,.textSearchError]')
+    FACETS_SEARCH=$(curl -s ${HOST}/api/facets-search/$JOB_ID | jq '[.hitCount,.textSearchError]')
     HIT_COUNT=$(echo ${FACETS_SEARCH} |  jq '.[0]')
     SEARCH_ERROR=$(echo ${FACETS_SEARCH} |  jq '.[1]')
 
@@ -38,10 +40,10 @@ if curl -s --head  --request GET $HOST | grep "200 OK" > /dev/null; then
     then
         text="Ops! There is something wrong with the RNAcentral sequence search. Please check the links below: \n
         \n
-        To check the status see: ${HOST}/${ENDPOINT}/job-status/$JOB_ID \n
+        To check the status see: ${HOST}/api/job-status/$JOB_ID \n
         The status output: ${INITIAL_STATUS} and ${FINAL_STATUS}. The expected status: started and success. \n
         \n
-        To check the results see: ${HOST}/${ENDPOINT}/job-results/$JOB_ID \n
+        To check the results see: ${HOST}/api/facets-search/$JOB_ID \n
         The result output: ${HIT_COUNT} and ${SEARCH_ERROR}. The expected result: 23 and false."
         escapedText=$(echo ${text} | sed 's/"/\"/g' | sed "s/'/\'/g" )
         json="{\"text\": \"$escapedText\"}"
