e3edc85e9eb4c99de9908b7c5253770dadccd52c
carlosribas <caduribas@gmail.com>
2020-09-02 12:45:18 +0100

    Add searches_per_month

--- sequence_search/producer/tests/test_show_searches.py
+++ sequence_search/producer/tests/test_show_searches.py
@@ -44,13 +44,16 @@ class ShowSearchesTestCase(AioHTTPTestCase):
         self.job_id_01 = str(uuid.uuid4())
         self.job_id_02 = str(uuid.uuid4())
         self.job_id_03 = str(uuid.uuid4())
+        self.date_01 = datetime.datetime.now()
+        self.date_02 = datetime.datetime.now() - datetime.timedelta(days=2)
+        self.date_03 = datetime.datetime.now() - datetime.timedelta(days=8)
 
         async with self.app['engine'].acquire() as connection:
             await connection.execute(
                 Job.insert().values(
                     id=self.job_id_01,
                     query='',
-                    submitted=datetime.datetime.now(),
+                    submitted=self.date_01,
                     status=JOB_STATUS_CHOICES.started
                 )
             )
@@ -59,7 +62,7 @@ class ShowSearchesTestCase(AioHTTPTestCase):
                 Job.insert().values(
                     id=self.job_id_02,
                     query='',
-                    submitted=datetime.datetime.now() - datetime.timedelta(days=2),
+                    submitted=self.date_02,
                     status=JOB_STATUS_CHOICES.started
                 )
             )
@@ -68,7 +71,7 @@ class ShowSearchesTestCase(AioHTTPTestCase):
                 Job.insert().values(
                     id=self.job_id_03,
                     query='',
-                    submitted=datetime.datetime.now() - datetime.timedelta(days=8),
+                    submitted=self.date_03,
                     status=JOB_STATUS_CHOICES.started
                 )
             )
@@ -81,6 +84,18 @@ class ShowSearchesTestCase(AioHTTPTestCase):
 
     @unittest_run_loop
     async def test_show_searches_success(self):
+        date_01 = self.date_01.strftime("%Y-%m")
+        date_02 = self.date_02.strftime("%Y-%m")
+        date_03 = self.date_03.strftime("%Y-%m")
+        searches_per_month = []
+
+        if date_01 == date_02 and date_02 == date_03:
+            searches_per_month = [{date_01: 3}]
+        elif date_01 == date_02 and date_02 != date_03:
+            searches_per_month = [{date_03: 1}, {date_01: 2}]
+        elif date_01 != date_02 and date_02 == date_03:
+            searches_per_month = [{date_03: 2}, {date_01: 1}]
+
         url = self.app.router["show-searches"].url_for()
         async with self.client.get(path=url) as response:
             assert response.status == 200
@@ -89,5 +104,6 @@ class ShowSearchesTestCase(AioHTTPTestCase):
             assert data == [
                 {"count": 3, "avg_time": 0, "search": "all"},
                 {"count": 1, "avg_time": 0, "search": "last-24-hours"},
-                {"count": 2, "avg_time": 0, "search": "last-week"}
+                {"count": 2, "avg_time": 0, "search": "last-week"},
+                searches_per_month
             ]
