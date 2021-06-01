45bf13ab34bc2d1a3bf121fd865fb6de2539866a
carlosribas <caduribas@gmail.com>
2021-06-01 10:18:56 +0100

    Send message in case of EBI endpoint failure

--- sequence_search/monitor/monitor.sh
+++ sequence_search/monitor/monitor.sh
@@ -18,6 +18,9 @@ DATABASE_AND_QUERY="{\"databases\": [\"$DATABASE\"], \"query\": \">sequence-sear
 # Ansible adds the correct floating_ip to the .conf-file
 HOST="http://$(echo $FLOATING_IP | sed 's/[][]//g'):8002"
 
+# Endpoint used to show facets
+EBI_ENDPOINT="https://www.ebi.ac.uk/ebisearch/ws/rest/rnacentral/seqtoolresults/"
+
 # First check if the $HOST is up
 if curl -s --head  --request GET $HOST | grep "200 OK" > /dev/null; then
     # Submit a job
@@ -42,21 +45,27 @@ if curl -s --head  --request GET $HOST | grep "200 OK" > /dev/null; then
         done
     fi
 
-    # Facets search
-    FACETS_SEARCH=$(curl -s ${HOST}/api/facets-search/$JOB_ID | jq '[.hitCount,.textSearchError]')
-    HIT_COUNT=$(echo ${FACETS_SEARCH} |  jq '.[0]')
-    SEARCH_ERROR=$(echo ${FACETS_SEARCH} |  jq '.[1]')
+    # Check if the EBI endpoint is up
+    if curl -s --head --request GET ${EBI_ENDPOINT}?toolid=nhmmer\&jobid=${JOB_ID} | grep "200 OK" > /dev/null; then
+        # Facets search
+        FACETS_SEARCH=$(curl -s ${HOST}/api/facets-search/$JOB_ID | jq '[.hitCount,.textSearchError]')
+        HIT_COUNT=$(echo ${FACETS_SEARCH} |  jq '.[0]')
 
-    # Send message in case of unexpected result
-    if [ "$STATUS" != "success" ] || [ "$HIT_COUNT" -gt 0 ] && [ "$SEARCH_ERROR" != "false" ]
-    then
-        text="Ops! There is something wrong with the RNAcentral sequence search. Please check the links below: \n
-        \n
-        To check the status see: ${HOST}/api/job-status/$JOB_ID \n
-        The job status is ${STATUS} (the expected status is success). \n
-        \n
-        To check the results see: ${HOST}/api/facets-search/$JOB_ID \n
-        The job results are ${HIT_COUNT} and ${SEARCH_ERROR}."
+        # Send message in case of unexpected result
+        if [ "$STATUS" != "success" ] || [ -z "$HIT_COUNT" ]
+        then
+            text="Ops! There is something wrong with the RNAcentral sequence search. Please check the links below: \n
+            \n
+            The job status is ${STATUS}. See: ${HOST}/api/job-status/$JOB_ID \n
+            \n
+            To check the results see: ${HOST}/api/facets-search/$JOB_ID"
+            escapedText=$(echo ${text} | sed 's/"/\"/g' | sed "s/'/\'/g" )
+            json="{\"text\": \"$escapedText\"}"
+            curl -s -d "payload=$json" $WEBHOOK_URL
+            exit
+        fi
+    else
+        text="Failed to load the sequence search facets. The EBI endpoint is down!"
         escapedText=$(echo ${text} | sed 's/"/\"/g' | sed "s/'/\'/g" )
         json="{\"text\": \"$escapedText\"}"
         curl -s -d "payload=$json" $WEBHOOK_URL
