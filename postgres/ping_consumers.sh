295b849aec085ad18496bae3f437e4e9f182ddcf
carlosribas <caduribas@gmail.com>
2021-08-27 12:52:24 +0100

    Send message on slack

--- postgres/ping_consumers.sh
+++ postgres/ping_consumers.sh
@@ -2,8 +2,10 @@
 # Script to change the status of the consumer that crashed.
 # If we can't ping the consumer, we should change its status to avoid the producer from trying to send jobs to it.
 
-dbname="producer"
-username="docker"
+# get variables
+set -o allexport
+source $PWD/.env
+set +o allexport
 
 # read IPs from file
 cat consumers.txt | while read output
@@ -14,11 +16,12 @@ do
     # update the status of the consumer if it is unreachable
     if [ $? -ne 0 ]; then
         # check the current status
-        status=(`psql -X -A -d $dbname -U $username -t -c "select status from consumer where ip='${output}'"`)
+        status=(`psql -X -A -d $DB -U $USER -t -c "select status from consumer where ip='${output}'"`)
 
-        # update if necessary
+        # update and notify if necessary
         if [ ${status} != "error" ]; then
-            psql -U $username -d $dbname -c "update consumer set status='error',job_chunk_id='unreachable' where ip='${output}'"
+            psql -U $USER -d $DB -c "update consumer set status='error',job_chunk_id='unreachable' where ip='${output}'"
+            curl -X POST -H 'Content-type: application/json' --data '{"text":"Unable to ping consumer '${output}'"}' $SLACK
         fi
     fi
 done
