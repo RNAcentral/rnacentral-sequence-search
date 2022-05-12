1a7f20848348bc378691f6df23d9c7f55499de35
carlosribas <caduribas@gmail.com>
2022-05-12 12:19:27 +0100

    Backup only the number of searches

--- postgres/backup.sh
+++ postgres/backup.sh
@@ -5,7 +5,7 @@ TIME=$(date +"%T")
 REMOVE=$(date +"%Y%m%d_%H" -d "5 days ago")
 
 echo "Removing old files"
-find /backup -name "producer_${REMOVE}*" -type f -exec rm -f {} \;
+find /backup -name "statistic_${REMOVE}*" -type f -exec rm -f {} \;
 
 echo "Backup the database"
-pg_dump -Fc producer -U docker > /backup/producer_${TODAY}_${TIME}.dump
+pg_dump -Fc producer -U docker --data-only -t statistic > /backup/statistic_${TODAY}_${TIME}.dump
