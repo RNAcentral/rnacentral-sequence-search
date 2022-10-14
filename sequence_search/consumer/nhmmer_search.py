5f8172dab501f91018a6b7f839cfef3c5c4d371c
carlosribas <caduribas@gmail.com>
2022-10-14 15:33:25 +0100

    Update e-value

--- sequence_search/consumer/nhmmer_search.py
+++ sequence_search/consumer/nhmmer_search.py
@@ -35,7 +35,7 @@ async def nhmmer_search(sequence, job_id, database):
     except ValueError:
         db_name = None
 
-    e_value = get_e_value(db_name) if db_name else 4106.44469
+    e_value = get_e_value(db_name) if db_name else 11179.11
 
     params = {
         'query': query_file_path(job_id, database),
