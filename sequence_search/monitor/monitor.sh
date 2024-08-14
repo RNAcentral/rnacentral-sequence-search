8725b01fd40ed5f17985b8ee196e7ba8534139b3
carlosribas <caduribas@gmail.com>
2024-08-14 20:45:39 +0100

    Get the number of available consumers and also how many jobs are in the queue

--- sequence_search/monitor/monitor.sh
+++ sequence_search/monitor/monitor.sh
@@ -54,10 +54,17 @@ if curl -s --head  --request GET $HOST | grep "200 OK" > /dev/null; then
 
         # After 50 minutes, if status is still started or pending, send a message
         if [ "$STATUS" == "started" ] || [ "$STATUS" == "pending" ]; then
-            text="The sequence search has not performed a search within a 50 minute period. \n
-            Either there are too many jobs to run, or the sequence search is having problems. \n
+            # SQL queries to check the sequence search service
+            CONSUMER_COUNT=$(ssh 192.168.0.6 "psql -U $USERNAME -d $DBNAME -t -c \"SELECT COUNT(ip) FROM consumer WHERE status='available';\"")
+            JOB_COUNT=$(ssh 192.168.0.6 "psql -U $USERNAME -d $DBNAME -t -c \"SELECT COUNT(id) FROM jobs WHERE status='started' AND submitted BETWEEN NOW() - INTERVAL '8 HOURS' AND NOW();\"")
+
+            # Trim whitespace
+            CONSUMER_COUNT=$(echo $CONSUMER_COUNT | xargs)
+            JOB_COUNT=$(echo $JOB_COUNT | xargs)
+
+            text="A sequence search test has not been performed within a 50 minute period. There are currently $CONSUMER_COUNT consumers available and $JOB_COUNT jobs in the queue. \n
             \n
-            Job status of this test: ${STATUS} - ${HOST}/api/facets-search/$JOB_ID"
+            When completed, the test search results will be available at: ${HOST}/api/facets-search/$JOB_ID"
             escapedText=$(echo ${text} | sed 's/"/\"/g' | sed "s/'/\'/g" )
             json="{\"text\": \"$escapedText\"}"
             curl -s -d "payload=$json" $WEBHOOK_URL
