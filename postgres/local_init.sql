bebd2fa4457e739ca8c38896e1bd8c8a31ebf822
carlosribas <caduribas@gmail.com>
2025-04-07 13:18:42 +0100

    Grant privileges

--- postgres/local_init.sql
+++ postgres/local_init.sql
@@ -1,6 +1,9 @@
 CREATE USER docker WITH ENCRYPTED PASSWORD 'example';
 CREATE DATABASE producer;
 GRANT ALL PRIVILEGES ON DATABASE producer TO docker;
+ALTER DATABASE producer OWNER TO docker;
 ALTER ROLE docker CREATEDB;
 CREATE DATABASE test_producer;
 GRANT ALL PRIVILEGES ON DATABASE test_producer TO docker;
+ALTER DATABASE test_producer OWNER TO docker;
+GRANT ALL ON schema public TO docker;
